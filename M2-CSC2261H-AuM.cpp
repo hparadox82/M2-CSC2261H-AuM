8aa2d41d611f415a35d6954fcd0f0ffb7beb9515
me <yuh@bruh.com>
2025-03-11 17:12:23 -0400

    function prototypes, declare array

--- M2-CSC2261H-AuM.cpp
+++ M2-CSC2261H-AuM.cpp
@@ -2,9 +2,17 @@
 //Project 1: Reverse array
 
 #include <iostream>
+using namespace std;
+
+//Function prototypes
+int* revArray(const int arr[], int size);
+void print(const int arr[], int size);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Declares array/size/values.
+    const int size = 10;
+    int origArray[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 }
 
