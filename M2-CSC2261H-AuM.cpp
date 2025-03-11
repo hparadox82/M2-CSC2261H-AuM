938e2c91afc2d53cf302c134b7dc74674a288b8a
me <yuh@bruh.com>
2025-03-11 17:23:25 -0400

    built file reader function (getData), implemented into main.

--- M2-CSC2261H-AuM.cpp
+++ M2-CSC2261H-AuM.cpp
@@ -25,6 +25,38 @@ char calculateLetterGrade(double average);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Reads students.txt and assigns to struct, so forth. If can't open file, displays error. File should be stored in project folder.
+    ifstream inputFile("students.txt");
+    if (!inputFile)
+    {
+        cout << "Error opening file! Filename should be 'students.txt' and be placed in the program folder.\n\n";
+        cout << "Please ensure the student data is structured like this:\n";
+        cout << "Number of Students Number of tests\nStudentName IDnumber TestScores(Each score is separated by a space)\n\n";
+        return 1;
+    }
+
+    int studentCnt, testsCnt;
+    student* students = getData(inputFile, studentCnt, testsCnt);
+    inputFile.close();
 }
 
+//reads filename "students.txt" that contains all info
+
+student* getData(ifstream& file, int& studentCnt, int& testsCnt)
+{
+    file >> studentCnt >> testsCnt;
+
+    student* students = new student[studentCnt];
+
+    for (int i = 0; i < studentCnt; i++)
+    {
+        file >> students[i].name >> students[i].id;
+        students[i].testScores = new double[testsCnt];
+        for (int j = 0; j < testsCnt; j++)
+        {
+            file >> students[i].testScores[j];
+        }
+    }
+
+    return students;
+}
